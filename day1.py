f6d82985541b03fed732257128e2f366dc934dc0
jatin_Byteblanket <jkaushik@byteblanket.com>
2020-08-30 17:12:17 +0530

    Day 1 code

---
+++ day1.py
@@ -0,0 +1,20 @@
+# Given A list of numbers find if two numbers(pair) exists such that sum of Numbers is equal to N
+# For example List = [5,-2,10,4,6,3,-6,15,1], N = 11 Answer would be 5,6 and other's
+
+list1 = [5,-2,10,4,6,3,-6,15,1]
+# o(n^2) Time || o(1) space Complexity
+"""def num(target_num):
+    for i in range(0,len(list1)):
+      first_num =  list1[i]
+      for j in range(i+1,len(list1)):
+        second_num = list1[j]
+        if first_num + second_num == 11:
+          print(first_num,second_num) """
+
+"""def num(target_num):
+    for i in range(0,len(list1)):
+      first_num =  list1[i]
+      for j in range(i+1,len(list1)):
+        second_num = list1[j]
+        if first_num + second_num == 11:
+          print(first_num,second_num) """
